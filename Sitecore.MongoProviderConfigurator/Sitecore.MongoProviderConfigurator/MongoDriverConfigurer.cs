6761c1b73bab6b4726061cfd4120dcfc6cb9236c
Vicent Galiana <vgaliana@clearpeople.com>
2016-12-06 20:14:26 +0100

    Force the use of ssl and enable tls 1.2

---
+++ Sitecore.MongoProviderConfigurator/Sitecore.MongoProviderConfigurator/MongoDriverConfigurer.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.MongoProviderConfigurator
+{
+    using System;
+    using Sitecore.Analytics.Pipelines.UpdateMongoDriverSettings;
+    using MongoDB.Driver;
+    using System.Security.Authentication;
+
+    // TODO: \App_Config\include\MongoDriverConfigurer.config created automatically when creating MongoDriverConfigurer class.
+
+    public class MongoDriverConfigurer : Analytics.Pipelines.UpdateMongoDriverSettings.UpdateMongoDriverSettingsProcessor
+    {
+        public override void UpdateSettings(UpdateMongoDriverSettingsArgs args)
+        {
+            args.MongoSettings.UseSsl = true;
+            args.MongoSettings.VerifySslCertificate = false;
+            args.MongoSettings.SslSettings = new SslSettings();
+            args.MongoSettings.SslSettings.EnabledSslProtocols = SslProtocols.Tls12;
+
+        }
+    }
+}
